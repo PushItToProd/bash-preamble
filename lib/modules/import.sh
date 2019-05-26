b49d74734b6b92638977fd2286968501d72cedca
pushittoprod <pushittoprod@gmail.com>
2019-05-26 11:17:31 -0700

    add relative imports

--- lib/modules/import.sh
+++ lib/modules/import.sh
@@ -1,14 +1,18 @@
 _preamble_resolve_module_path() {
-  local -r path="$1"
-  local -r actual_path="${path/.//}.mod.bash"
+  local -r script_dir="$1"
+  local -r module_path="$2"
+  local -r actual_path="$script_dir/${module_path/.//}.mod.bash"
   printf "%s" "$actual_path"
 }
 
 importmod() {
   local -r mod="$1"
-  local -r module_path="$(_preamble_resolve_module_path "$mod")"
+  local -r script_path="${BASH_SOURCE[1]}"
+  local -r script_dir="$(dirname "$script_path")"
+  local -r module_path="$(_preamble_resolve_module_path "$script_dir" "$mod")"
   if [[ ! -e "$module_path" ]]; then
-    echo "${BASH_SOURCE[1]}: line ${BASH_LINENO[0]}: module '$mod' not found"
+    echo "$script_path: line ${BASH_LINENO[0]}: module '$mod' not found (searched $module_path)"
     return 1
   fi
+  source "$module_path"
 }
