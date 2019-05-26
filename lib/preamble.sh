2a6b980c44364f7d3a8a52d09da4cc7a15bc170e
pushittoprod <pushittoprod@gmail.com>
2019-05-26 10:13:38 -0700

    resolve full path to preamble.sh for sourcing modules and encapsulate setup/teardown in functions

--- lib/preamble.sh
+++ lib/preamble.sh
@@ -1,6 +1,41 @@
+##### Helper Methods #####
+
+# Get the full directory name of the given file, resolving any symlinks inthe
+# process.
+safe_dirname() {
+  cd -P "$(dirname "$1")" >/dev/null 2>&1
+  pwd
+}
+
+is_symlink() {
+  [[ -h "$1" ]]
+}
+
+is_absolute_path() {
+  [[ "$1" == /* ]]
+}
+
+is_relative_path() {
+  ! is_absolute_path "$1"
+}
+
+# Resolve nested and relative symlinks.
+# (based on https://stackoverflow.com/a/246128)
+safe_readlink() {
+  local SOURCE="$1"
+  local DIR
+  while is_symlink "$SOURCE"; do
+    DIR="$(safe_dirname "$SOURCE")"
+    SOURCE="$(readlink "$SOURCE")"
+    if is_relative_path "$SOURCE"; then
+      SOURCE="$DIR/$SOURCE"
+    fi
+  done
+  printf "$(safe_dirname "$SOURCE")"
+}
+
 # Set the given flags
 _preamble_set_flags() {
-  declare -ag _PREAMBLE_SET_FLAGS=()
   for flag in "$@"; do
     if [[ ! -o "$flag" ]]; then
       _PREAMBLE_SET_FLAGS+=("$flag")
@@ -13,13 +48,27 @@ _preamble_revert_flags() {
   for flag in "${_PREAMBLE_SET_FLAGS[@]}"; do
     set +o "$flag"
   done
+}
+
+_preamble_setup() {
+  # Find the real path to preamble.sh.
+  declare -g _PREAMBLE_SOURCE_PATH="$(safe_dirname ${BASH_SOURCE[0]})"
+
+  # Create the array used by _preamble_set_flags.
+  declare -ag _PREAMBLE_SET_FLAGS=()
+  _preamble_set_flags errexit nounset pipefail
+}
+
+_preamble_teardown() {
+  _preamble_revert_flags
   unset -v _PREAMBLE_SET_FLAGS
   unset -f _preamble_set_flags
   unset -f _preamble_revert_flags
 }
 
-_preamble_set_flags errexit nounset pipefail
 
-source "$(dirname "${BASH_SOURCE[0]}")/executables.sh"
+_preamble_setup
 
-_preamble_revert_flags
+source "$_PREAMBLE_SOURCE_PATH/executables.sh"
+
+_preamble_teardown
